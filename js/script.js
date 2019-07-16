42415ac551a9def495371e118c56427c53c47fdd
Hanna Bieri <hanna.bieri@code.berlin>
2019-07-16 09:10:54 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('mydetails').addEventListener('click', function(e) {
+    e.preventDefault();
+});
+
+
+
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");
+	});
