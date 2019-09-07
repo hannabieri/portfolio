267abc87bd30bfaae6aeae8e4e56801ba4ea4a0f
Hanna Bieri <hanna.bieri@code.berlin>
2019-09-07 19:33:09 +0200

    smooth loading, type on first page, arrow with delay, deleting index-navigation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// document.getElementById('mydetails').addEventListener('click', function(e) {
-//     e.preventDefault();
-// });
-//
-//
-//
-// 	// Wait for window load
-// 	$(window).load(function() {
-// 		// Animate loader off screen
-// 		$(".se-pre-con").fadeOut("slow");
-// 	});
-
-
-
 function escapeAbout() {
   document.addEventListener("keydown", function(event) {
     if (event.which === 27) {
@@ -24,19 +10,19 @@ function escapeAbout() {
 }
 
 
-var speed = 'slow';
-
-$('html, body').hide();
-
-$(document).ready(function() {
-  $('html, body').fadeIn(speed, function() {
-    $('a[href], button[href]').click(function(event) {
-      var url = $(this).attr('href');
-      if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
-      event.preventDefault();
-      $('html, body').fadeOut(speed, function() {
-        window.location = url;
-      });
-    });
-  });
-});
+// var speed = 'slow';
+//
+// $('html, body').hide();
+//
+// $(document).ready(function() {
+//   $('html, body').fadeIn(speed, function() {
+//     $('a[href], button[href]').click(function(event) {
+//       var url = $(this).attr('href');
+//       if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
+//       event.preventDefault();
+//       $('html, body').fadeOut(speed, function() {
+//         window.location = url;
+//       });
+//     });
+//   });
+// });
